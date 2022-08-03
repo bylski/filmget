9153d572161256b6752bd867b23bef5e74c85db6
Bylski <patryk.przybylski185@gmail.com>
2022-08-03 23:42:16 +0200

    header positioning changes

--- pages/home.tsx
+++ pages/home.tsx
@@ -5,14 +5,19 @@ import SearchInput from "../components/UI/SearchInput";
 const Home: React.FC = () => {
   return (
     <header className={styles["header"]}>
-      <section className={styles["header__img"]}></section>
-      <section className={styles["header__content"]}>
-        <div className={styles["header__text-container"]}>
-          <p className={styles["header__text"]}>
-            <span>Welcome.</span><br/>Millions of movies, TV shows and people to discover. Explore now.
-          </p>
+      <section className={styles["header__img"]}>
+        <div className={styles["header__content-wrapper"]}>
+        <div className={styles["header__content"]}>
+          <div className={styles["header__text-container"]}>
+            <p className={styles["header__text"]}>
+              <span>Welcome.</span>
+              <br />
+              Millions of movies, TV shows and people to discover. Explore now.
+            </p>
+          </div>
+          <SearchInput placeholder="Search for movies and tv series..." />
         </div>
-        <SearchInput placeholder="Search for movies and tv series..."/>
+        </div>
       </section>
     </header>
   );
