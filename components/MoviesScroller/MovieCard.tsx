5f026204c52053c64aaf945543ebc3706ca7e355
Bylski <patryk.przybylski185@gmail.com>
2022-08-04 19:39:42 +0200

    main page additions

--- components/MoviesScroller/MovieCard.tsx
+++ components/MoviesScroller/MovieCard.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import styles from "./styles/MovieCard.module.scss";
 import RatingIcon from "./RatingIcon";
+import Image from "next/image";
 
 const MovieCard: React.FC<{ imgSrc: string }> = (props) => {
   return (
     <div className={styles["movie-img__container"]}>
-      <img className={styles["movie-img"]} src={props.imgSrc}></img>
+      <Image width="600" height="900" className={styles["movie-img"]} src={props.imgSrc}></Image>
       <div className={styles["movie-rating__container"]}>
         <RatingIcon className={styles["rating-icon"]} />
         <p className={styles["rating-average"]}>7.2</p>
