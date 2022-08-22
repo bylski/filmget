b5b8fccbec52145358e2f4f23c0464d28b32c2e7
Bylski <bylski.patryk@gmail.com>
2022-08-22 18:55:42 +0200

    Implemented navbar link higlight based on current route

--- components/Layout/Navbar/NavbarLinks.tsx
+++ components/Layout/Navbar/NavbarLinks.tsx
@@ -1,9 +1,10 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import styles from "../styles/Navbar.module.scss";
 import { navLinksVariants } from "../../../utils/AnimationVariants.ts";
 import { navMenuVariants } from "../../../utils/AnimationVariants.ts";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const NavbarLinks: React.FC<{ navMenuShow: boolean }> = (props) => {
   let animateNavLinks: string = "";
@@ -13,21 +14,31 @@ const NavbarLinks: React.FC<{ navMenuShow: boolean }> = (props) => {
     animateNavLinks = "show";
   }
 
+  const router = useRouter();
+
+  const [selectedLink, setSelectedLink] = useState<string | null>()
+  useEffect(() => {
+    const slicedPath = router.pathname.slice(1, router.pathname.length);
+    setSelectedLink(slicedPath)
+  }, [router.pathname])
+
+  const linkBasicClass = styles["link__container"];
+  const linkActiveClass = `${styles["link__container"]} ${styles['link--active']}`
 
   return (
     <Fragment>
       <ul className={styles["nav-links__main"]}>
-      <li className={styles["link__container"]}>
+      <li className={selectedLink === "movies" ? linkActiveClass : linkBasicClass}>
           <Link href="/movies">
             <a className={styles["link"]}>MOVIES</a>
           </Link>
         </li>
-        <li className={styles["link__container"]}>
+        <li className={selectedLink === "series" ? linkActiveClass : linkBasicClass}>
           <Link href="/series">
             <a className={styles["link"]}>SERIES</a>
           </Link>
         </li>
-        <li className={styles["link__container"]}>
+        <li className={selectedLink === "people" ? linkActiveClass : linkBasicClass}>
           <Link href="/people">
             <a className={styles["link"]}>PEOPLE</a>
           </Link>
@@ -40,17 +51,17 @@ const NavbarLinks: React.FC<{ navMenuShow: boolean }> = (props) => {
         animate={animateNavLinks}
         className={styles["nav-links__collapsable"]}
       >
-        <motion.li variants={navLinksVariants} className={styles["link__container"]}>
+        <motion.li variants={navLinksVariants} className={selectedLink === "movies" ? linkActiveClass : linkBasicClass}>
           <Link href="/movies">
             <a className={styles["link"]}>MOVIES</a>
           </Link>
         </motion.li>
-        <motion.li variants={navLinksVariants} className={styles["link__container"]}>
+        <motion.li variants={navLinksVariants} className={selectedLink === "series" ? linkActiveClass : linkBasicClass}>
           <Link href="/series">
             <a className={styles["link"]}>SERIES</a>
           </Link>
         </motion.li>
-        <motion.li variants={navLinksVariants} className={styles["link__container"]}>
+        <motion.li variants={navLinksVariants} className={selectedLink === "people" ? linkActiveClass : linkBasicClass}>
           <Link href="/people">
             <a className={styles["link"]}>PEOPLE</a>
           </Link>
