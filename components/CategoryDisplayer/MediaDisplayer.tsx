b0e2bf0480dd8a0922b5e099b73aa68b75b150f3
Bylski <bylski.patryk@gmail.com>
2022-08-22 23:31:24 +0200

    Created a reusable switcher component

--- components/CategoryDisplayer/MediaDisplayer.tsx
+++ components/CategoryDisplayer/MediaDisplayer.tsx
@@ -1,6 +1,7 @@
-import axios from "axios";
 import React from "react";
 import styles from "./styles/MediaDisplayer.module.scss";
+import RatingIcon from "../Icons/RatingIcon";
+import Switcher from "../UI/Switcher";
 
 const mediaCard = (
   <div className={styles["media__card"]}>
@@ -11,34 +12,35 @@ const mediaCard = (
       />
     </div>
     <div className={styles["media__info"]}>
-      <p className={styles["media__info-title"]}>Stranger Things 3</p>
+      <div className={styles["media__rating-container"]}>
+        <RatingIcon className={styles["media__rating-icon"]}></RatingIcon>
+        <p className={styles["media__rating-average"]}>6.5</p>
+      </div>
+      <p className={styles["media__title"]}>Stranger Things 3</p>
     </div>
   </div>
 );
 
-const MediaDisplayer: React.FC<{mediaType: string}> = (props) => {
-
-
+const MediaDisplayer: React.FC<{ mediaType: string }> = (props) => {
   return (
     <section className={styles["media-displayer"]}>
       <main className={styles["media-displayer__card"]}>
         <header className={styles["media-displayer__header"]}>
-          <p className={styles["media-displayer__header-text"]}>
-            {`Trending ${props.mediaType}`} 
-          </p>
+          <p className={styles["media-displayer__header-text"]}></p>
+          <Switcher switches={[{switchName: "Trending"}, {switchName: "Upcoming"}, {switchName: "Top Rated"}]}/>
         </header>
         <main className={styles["media-displayer__content"]}>
-        <div className={styles["media__card"]}>
-    <div className={styles["media__img-container"]}>
-      <img
-        className={styles["media__img"]}
-        src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/cvJ13HfBb6PXEzhpuMcAdUjuQq1.jpg"
-      />
-    </div>
-    <div className={styles["media__info"]}>
-      <p className={styles["media__info-title"]}>Stranger Things 3</p>
-    </div>
-  </div>
+          <div className={styles["media__card"]}>
+            <div className={styles["media__img-container"]}>
+              <img
+                className={styles["media__img"]}
+                src="https://www.themoviedb.org/t/p/w600_and_h900_bestv2/cvJ13HfBb6PXEzhpuMcAdUjuQq1.jpg"
+              />
+            </div>
+            <div className={styles["media__info"]}>
+              <p className={styles["media__info-title"]}>Stranger Things 3</p>
+            </div>
+          </div>
           {mediaCard}
           {mediaCard}
           {mediaCard}
---
+++ components/UI/Switcher.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import styles from "./styles/Switcher.module.scss";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const Switcher: React.FC<{ switches: { switchName: string }[] }> = (props) => {
+  const [currentlySelected, setCurentlySelected] = useState(0);
+  const switchClickedHandler = (e: React.MouseEvent) => {
+    e.preventDefault();
+    const target = e.currentTarget as HTMLUListElement;
+    const slicedId = target.id.slice(6, target.id.length);
+    setCurentlySelected(parseInt(slicedId));
+  };
+
+  const allSwitches = props.switches.map((currentSwitch, i) => {
+    return (
+      <li
+        className={
+          i === props.switches.length - 1
+            ? `${styles["switcher-element"]} ${styles["last"]}`
+            : styles["switcher-element"]
+        }
+        id={`switch${i}`}
+        key={`switch${i}`}
+        onClick={switchClickedHandler}
+      >
+        <h1 className={styles["switcher-element__text"]}>
+          {currentSwitch.switchName}
+        </h1>
+        {currentlySelected === i ? (
+          <motion.div
+            layoutId="background"
+            className={styles["switcher-element__animated-bg"]}
+          ></motion.div>
+        ) : null}
+      </li>
+    );
+  });
+
+  return <ul className={styles["switcher"]}>{allSwitches}</ul>;
+};
+
+export default Switcher;
