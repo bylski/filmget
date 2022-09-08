eea3cbbeb612e6573be14c09030dcf849b16110c
Bylski <bylski.patryk@gmail.com>
2022-09-08 22:44:19 +0200

    Create login and register routes and add base styling to their components

---
+++ pages/login/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RegisterLogin from "../../components/RegisterLogin/RegisterLogin";
+
+const Login: React.FC = () => {
+    return (
+        <RegisterLogin headerText="Log In" />
+    )
+}
+
+export default Login;
---
+++ pages/register/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import RegisterLogin from "../../components/RegisterLogin/RegisterLogin";
+
+const Register: React.FC = () => {
+    return (
+        <RegisterLogin headerText="Create Account"/>
+    )
+}
+
+export default Register;
