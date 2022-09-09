1d9fa6c1b5775c063f6a6ec049dc2fc1b1c2ae4e
Bylski <bylski.patryk@gmail.com>
2022-09-10 00:16:31 +0200

    Add carousel effect to background images in login and register routes

--- components/RegisterLogin/RegisterLogin.tsx
+++ components/RegisterLogin/RegisterLogin.tsx
@@ -1,15 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./styles/RegisterLogin.module.scss";
 
-const RegisterLogin: React.FC<{headerText: string}> = (props) => {
+const urls = [
+  "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/wcKFYIiVDvRURrzglV9kGu7fpfY.jpg",
+  "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/68sanslplXryiJWzv0uMuXjJBmB.jpg",
+  "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/ocUp7DJBIc8VJgLEw1prcyK1dYv.jpg",
+  "https://image.tmdb.org/t/p/w1920_and_h800_bestv2/27Mj3rFYP3xqFy7lnz17vEd8Ms.jpg",
+  "https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/yQTQL9pliY6vpRt8HkjUJrKymBb.jpg",
+];
+
+const RegisterLogin: React.FC<{ headerText: string }> = (props) => {
+  const [currentUrlIndex, setUrlIndex] = useState(0);
+  const [imgFadeOut, setImgFadeOut] = useState(false);
+
+  let carouselInterval: any = undefined;
+  let fadeInTimeout: any = undefined;
+  useEffect(() => {
+    if (carouselInterval) {
+      clearInterval(carouselInterval);
+    }
+    carouselInterval = setInterval(() => {
+      setUrlIndex((prev) => {
+        if (prev === urls.length - 1) {
+          return 0;
+        }
+        return prev + 1;
+      });
+    }, 10000);
+  }, []);
+
   return (
     <main className={styles["register-login"]}>
       <div className={styles["register-login__card"]}>
         <div className={styles["card__img-section"]}>
-          <img
-            className={styles["card__img"]}
-            src="https://www.themoviedb.org/t/p/w1920_and_h800_multi_faces/wcKFYIiVDvRURrzglV9kGu7fpfY.jpg"
-          ></img>
+          {urls.map((url, i) => {
+            if (currentUrlIndex - 1 === i) {
+              return (
+                <img
+                  className={`${styles["card__img"]} ${styles["fade-out"]}`}
+                  src={url}
+                ></img>
+              );
+            }
+            if (currentUrlIndex === i) {
+              return (
+                <img
+                  className={`${styles["card__img"]} ${styles["active"]}`}
+                  src={url}
+                ></img>
+              );
+            }
+            return <img className={styles["card__img"]} src={url}></img>;
+          })}
         </div>
         <div className={styles["card__form-section"]}>
           <form className={styles["card__form"]}>
